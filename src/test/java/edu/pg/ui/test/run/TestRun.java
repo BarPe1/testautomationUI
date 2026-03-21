03b46b89a4568bb0396b0c5b4f22a75db4a0e332
BarPe1 <37332946+BarPe1@users.noreply.github.com>
2026-03-21 16:45:46 +0100

    Update TestRun.java
    Added to run unit test

--- src/test/java/edu/pg/ui/test/run/TestRun.java
+++ src/test/java/edu/pg/ui/test/run/TestRun.java
@@ -1,8 +1,12 @@
 package edu.pg.ui.test.run;
 
+import edu.pg.ui.test.AssertUnitClass;
 import edu.pg.ui.test.BasePage;
 import edu.pg.ui.test.LoginPage;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -27,21 +31,22 @@ public class TestRun extends BasePage {
 
 
     public LoginPage loginPage;
+    public AssertUnitClass assertUnitClass;
 
     @BeforeMethod(alwaysRun = true)
     @Override
     public void setUp() {
-        super.setUp(); 
-       
+        super.setUp();
+
         loginPage = new LoginPage(driver);
-     
+        assertUnitClass = new AssertUnitClass(driver);
         PageFactory.initElements(driver, loginPage);
     }
 
-    @Test(groups = "smoke")
-    @Parameters({"user", "pass"})
+   // @Test(groups = "smoke")
+   // @Parameters({"user", "pass"})
     public void userTriesToLoginToSauceDemo(String user, String pass) throws Exception {
-      
+
         driver.get(SAUCE_DEMO_URL);
 
         loginPage.enterUsername(user);
@@ -50,6 +55,14 @@ public class TestRun extends BasePage {
         TimeUnit.SECONDS.sleep(2);
         loginPage.clickLoginButton();
         TimeUnit.SECONDS.sleep(2);
+    }
+
+    @Test(groups = "regression")
+    public void shouldBeErrorPopUp() throws Exception {
+        driver.get("https://saucedemo.com");
+        loginPage.clickLoginButton();
+        log.info("Error:{}", loginPage.getTextError());
+        Assertions.assertEquals("Epic sadface: Username is required", loginPage.getTextError(),"Error pop-ups");
     }
 
 }
