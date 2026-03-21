438486b4a1b6c419d1381c010466b153cb2a569d
BarPe1 <37332946+BarPe1@users.noreply.github.com>
2026-03-21 14:12:54 +0100

    Update LoginPage.java
    LoginPage page object model class has been modified.

--- src/main/java/edu/pg/ui/test/LoginPage.java
+++ src/main/java/edu/pg/ui/test/LoginPage.java
@@ -2,11 +2,21 @@ package edu.pg.ui.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.IOException;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 @Slf4j
 public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 
 //    //Page Object Model for alphanumeric fields
 //    private static final By LOGO = By.xpath("//*[@id='root']/div/div[@class='login_logo']");
@@ -23,6 +33,7 @@ public class LoginPage extends BasePage {
 
     public LoginPage clickLoginButton() {
         log.info("Click on the Login Button");
+        click(loginButton);
         loginButton.click();
         return this;
     }
@@ -39,9 +50,25 @@ public class LoginPage extends BasePage {
         return this;
     }
 
-    public String getLogoTextFromWebsiteHeader() {
+    public String getLogoTextFromWebsiteHeader() throws IOException {
         log.info("Get logo text from the header from the current website{}", logo.getText());
+        takeScreenshot("logo.png");
         return logo.getText();
+    }
+
+    public String getLogoTextForSmartLocator() {
+        WebElement element = driver.findElement(with(By.className("login_logo")).above(username)); //Smart selector
+        return String.valueOf(element.getText());
+    }
+
+    //Basic Shift-Left
+    protected boolean isDisplayed(WebElement element) {
+        try {
+            return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+        } catch (Exception e) {
+            log.error("Get message from catch exception {}", e.getMessage());
+            return false;
+        }
     }
 
 }
