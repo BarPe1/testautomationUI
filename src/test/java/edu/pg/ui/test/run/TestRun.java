7223a0297c60f2ed0ff61e5ef3ffea810f6ae02c
BarPe1 <37332946+BarPe1@users.noreply.github.com>
2026-03-21 14:14:31 +0100

    Update TestRun.java
    Update TestRun runner class

--- src/test/java/edu/pg/ui/test/run/TestRun.java
+++ src/test/java/edu/pg/ui/test/run/TestRun.java
@@ -3,30 +3,53 @@ package edu.pg.ui.test.run;
 import edu.pg.ui.test.BasePage;
 import edu.pg.ui.test.LoginPage;
 import lombok.extern.slf4j.Slf4j;
-import org.testng.Assert;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class TestRun extends BasePage {
+//
+//    //@Test
+//    public void shouldOpenGoogleWebsite() {
+//        driver.get(GOOGLE);
+//
+//        String title = driver.getTitle();
+//        log.info("This Google website has title: {}", title);
+//
+//        Assert.assertNotNull(title);
+//        Assert.assertTrue(true, String.valueOf(title.contains("Google")));
+//    }
+//
+
+
     public LoginPage loginPage;
 
-    @Test
-    public void shouldOpenGoogleWebsite() {
-        driver.get(GOOGLE);
-
-        String title = driver.getTitle();
-        log.info("This Google website has title: {}", title);
-
-        Assert.assertNotNull(title);
-        Assert.assertTrue(true, String.valueOf(title.contains("Google")));
+    @BeforeMethod(alwaysRun = true)
+    @Override
+    public void setUp() {
+        super.setUp(); 
+       
+        loginPage = new LoginPage(driver);
+     
+        PageFactory.initElements(driver, loginPage);
     }
 
-    @Test
-    public void userTriesToLoginToSauceDemo() {
+    @Test(groups = "smoke")
+    @Parameters({"user", "pass"})
+    public void userTriesToLoginToSauceDemo(String user, String pass) throws Exception {
+      
         driver.get(SAUCE_DEMO_URL);
-        loginPage.getLogoTextFromWebsiteHeader();
 
+        loginPage.enterUsername(user);
+        TimeUnit.SECONDS.sleep(2);
+        loginPage.enterPasswordToSauceDemoWebsiteOnTestEnv(pass);
+        TimeUnit.SECONDS.sleep(2);
+        loginPage.clickLoginButton();
+        TimeUnit.SECONDS.sleep(2);
     }
 
 }
-
